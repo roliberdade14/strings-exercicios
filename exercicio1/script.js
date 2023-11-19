4f2d8586f021181bf5314cff9fcd29cf59cbe465
Roberto Jesus <roliberdade1433@gmail.com>
2023-11-19 18:00:38 -0300

    envio strings-exercicios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,3 +1,10 @@
-function imprimirComidasFavoritas (){
+ function imprimirComidasFavoritas (){
+    const nome = prompt("Qual seu nome")
+    let imprimirComida1 = prompt("Qual sua comida favorita Nº1")
+    let imprimirComida2 = prompt("Qual sua comida favorita Nº2")
+    let imprimirComida3 = prompt("Qual sua comida favorita Nº3")
 
-}
+    console.log(`Estas são as comidas favoritas de :${nome}\n${imprimirComida1}\n${imprimirComida2}\n${imprimirComida3}`)
+
+} 
+imprimirComidasFavoritas()
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,31 @@
-//Crie a const para a frase aqui
+const minhaFrase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"';
+
+function substituirCores(frase) {
+    const fraseAlterada = frase.replace(/verde/g, 'rosa').replace(/azul/g, 'laranja')
+    console.log(minhaFrase)
+    console.log(fraseAlterada)
+            
+
+const inicioDaParteEmMinusculas = minhaFrase.indexOf('mas não deixe o gato sair');
+
+
+const parteAntes = minhaFrase.substring(0, inicioDaParteEmMinusculas);
+
+
+const parteEmMinusculas = minhaFrase.substring(inicioDaParteEmMinusculas, inicioDaParteEmMinusculas + 'mas não deixe o gato sair'.length);
+
+
+const parteDepois = minhaFrase.substring(inicioDaParteEmMinusculas + 'mas não deixe o gato sair'.length);
+
+
+const parteEmMaiusculas = parteEmMinusculas.toUpperCase();
+
+const fraseFinal = parteAntes + parteEmMaiusculas + parteDepois;
+
+console.log(fraseFinal);
+
+
+}
+
+// Chamando a função e passando a constante como argumento
+substituirCores(minhaFrase);
